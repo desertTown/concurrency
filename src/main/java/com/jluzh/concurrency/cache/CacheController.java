858a902c0c330ee9b0603f5409c080bca1bd7a94
Evan Huang <evan.huang@oocl.com>
2018-10-08 08:48:59 +0800

    【12.3】高并发之缓存-redis的使用

---
+++ src/main/java/com/jluzh/concurrency/cache/CacheController.java
@@ -0,0 +1,26 @@
+package com.jluzh.concurrency.cache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cache")
+public class CacheController {
+
+    @Autowired
+    private RedisClient redisClient;
+
+    @GetMapping("/set")
+    public String set(@RequestParam("k") String k, @RequestParam("v") String v) throws Exception {
+        redisClient.set(k, v);
+        return "SUCCESS";
+    }
+
+    @GetMapping("/get")
+    public String get(@RequestParam("k") String k) throws Exception {
+        return redisClient.get(k);
+    }
+}
